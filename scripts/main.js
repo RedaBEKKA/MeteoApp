0748106492215938f788f890d832b9cccb86e40f
unknown <reda.bekka@gmail.com>
2020-11-01 21:11:07 +0100

    Modifié les valeur statiques de l'app et intégrer les données de l'api sur notre application

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 const CLEFAPI = "b93bf2e390b99c07d176f7c11e5b0012";
 let resultAPI;
 
+const temps = document.querySelector('.temps');
+const temperature = document.querySelector('.temperature');
+const localisation = document.querySelector('.localisation');
+const heure = document.querySelectorAll('.heure-nom-prevision');
+const tempPourH = document.querySelectorAll('.heure-prevision-valeur');
+
 // ce qui ns interesse ce longitude et latitude car on va extraire et le mettre ds des variable et on fait appel a l'API avec cette posiition
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(position => {
@@ -21,6 +27,37 @@ function AppelAPI(long,lat){
         return response.json();
     })
     .then((data)=>{
-        console.log(data);
+        //console.log(data);
+        resultAPI = data;
+
+        temps.innerText = resultAPI.current.weather[0].description;
+        temperature.innerText = `${Math.trunc(resultAPI.current.temp)}°` //trunc:pour enlever les chiffres aprés la virgule
+        localisation.innerText = resultAPI.timezone;
+
+        //les heures , par tranche de trois avec leur température
+        let heureActuelle = new Date().getHours();
+
+
+        for (let i = 0; i < heure.length; i++) {
+            
+            let heureIncr = heureActuelle+i*3;
+
+            if(heureIncr>24){
+                heure[i].innerText = `${heureIncr-24} h`;
+            }else if(heureIncr === 24){
+                heure[i].innerText = `00 h`;
+            }else{
+                heure[i].innerText = `${heureIncr} h`;
+            }
+            
+        }
+
+        //temp pour 3h
+
+        for (let j = 0; j < tempPourH.length; j++) {
+            tempPourH[j].innerText = `${Math.trunc(resultAPI.hourly[j*3].temp)}°`
+            
+        }
+
     })
 }
