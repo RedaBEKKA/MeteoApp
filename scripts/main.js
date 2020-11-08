15b94bfe613f6b73fe35081a25bc38fe54d5adbd
unknown <reda.bekka@gmail.com>
2020-11-08 22:24:54 +0100

    Ajout de l'icone de chargement ainsi les températures par jours

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,10 @@ const temperature = document.querySelector('.temperature');
 const localisation = document.querySelector('.localisation');
 const heure = document.querySelectorAll('.heure-nom-prevision');
 const tempPourH = document.querySelectorAll('.heure-prevision-valeur');
+const jour = document.querySelectorAll('.jour-prevision-nom');
+const tempJours = document.querySelectorAll('.jour-prevision-temps');
+const imgIcon = document.querySelector('.logo-meteo');
+const chargement = document.querySelector('.overlay-icon-chargement')
 
 // ce qui ns interesse ce longitude et latitude car on va extraire et le mettre ds des variable et on fait appel a l'API avec cette posiition
 if(navigator.geolocation){
@@ -30,11 +34,11 @@ function AppelAPI(long,lat){
         return response.json();
     })
     .then((data)=>{
-        //console.log(data);
+        console.log(data);
         resultAPI = data;
 
         temps.innerText = resultAPI.current.weather[0].description;
-        temperature.innerText = `${Math.trunc(resultAPI.current.temp)}°` //trunc:pour enlever les chiffres aprés la virgule
+        temperature.innerText = `${Math.trunc(resultAPI.current.temp/10)}°` //trunc:pour enlever les chiffres aprés la virgule
         localisation.innerText = resultAPI.timezone;
 
         //les heures , par tranche de trois avec leur température
@@ -58,9 +62,30 @@ function AppelAPI(long,lat){
         //temp pour 3h
 
         for (let j = 0; j < tempPourH.length; j++) {
-            tempPourH[j].innerText = `${Math.trunc(resultAPI.hourly[j*3].temp)}°`
+            tempPourH[j].innerText = `${Math.trunc(resultAPI.hourly[j*3].temp/10)}°`
             
         }
+
+        //Trois premieres lettres des jours
+        for(let k=0; k<tabJourOrdre.length;k++){
+            jour[k].innerText= tabJourOrdre[k].slice(0,3);
+        }
+
+        //temp par jour
+
+        for(let m=0;m<7;m++){
+            tempJours[m].innerText = `${Math.trunc(resultAPI.daily[m+1].temp.day/10)}°`
+        }
+
+        //Icone dynamique :
+        if(heureActuelle>=6 && heureActuelle<21){
+            imgIcon.src = `ressources/jour/${resultAPI.current.weather[0].icon}.svg`
+        }else{
+            imgIcon.src = `ressources/nuit/${resultAPI.current.weather[0].icon}.svg`
+        }
+
+
+        chargement.classList.add('disparition')
 
     })
 }
